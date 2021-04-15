087fdb93089f35afbeff9c1be0fa0ba12ec0b406
MarcelloVendruscolo <marcello.pietro@hotmail.com>
2021-04-15 14:59:20 +0300

    Formatted and cleaned up the code

--- feedForward_backprop_neuralNetwork/load_mnist.py
+++ feedForward_backprop_neuralNetwork/load_mnist.py
@@ -2,33 +2,30 @@ import numpy as np
 import imageio
 import glob
 
-def load_mnist():
-    # Loads the MNIST dataset from png images
- 
-    NUM_LABELS = 10        
-    # create list of image objects
-    test_images = []
-    test_labels = []    
+def load_mnist(folder_path):
     
-    for label in range(NUM_LABELS):
-        for image_path in glob.glob("MNIST/Test/" + str(label) + "/*.png"):
-            image = imageio.imread(image_path)
-            test_images.append(image)
-            letter = [0 for _ in range(0,NUM_LABELS)]    
-            letter[label] = 1
-            test_labels.append(letter)  
-            
-    # create list of image objects
+    NUM_LABELS = 10 # Number of different classes (digits ranging from 0 to 9)
+
     train_images = []
-    train_labels = []    
-    
+    train_labels = []
+    test_images = []
+    test_labels = []
+
     for label in range(NUM_LABELS):
-        for image_path in glob.glob("MNIST/Train/" + str(label) + "/*.png"):
+        for image_path in glob.glob(str(folder_path) + "/Train/" + str(label) + "/*.png"):
             image = imageio.imread(image_path)
             train_images.append(image)
             letter = [0 for _ in range(0,NUM_LABELS)]    
             letter[label] = 1
-            train_labels.append(letter)                  
+            train_labels.append(letter)  
+    
+    for label in range(NUM_LABELS):
+        for image_path in glob.glob(str(folder_path) + "/Test/" + str(label) + "/*.png"):
+            image = imageio.imread(image_path)
+            test_images.append(image)
+            letter = [0 for _ in range(0,NUM_LABELS)]    
+            letter[label] = 1
+            test_labels.append(letter)                  
             
     X_train= np.array(train_images).reshape(-1,784)/255.0
     Y_train= np.array(train_labels)
